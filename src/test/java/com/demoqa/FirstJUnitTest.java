e219586ab4ef846cfbbc23facb295ba45f7c49f1
Forselo <forelo@gmail.com>
2023-03-28 21:45:10 +0300

    remove build

--- src/test/java/com/demoqa/FirstJUnitTest.java
+++ src/test/java/com/demoqa/FirstJUnitTest.java
@@ -1,4 +1,11 @@
 package com.demoqa;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FirstJUnitTest {
+    @Test
+    void firstTest(){
+        Assertions.assertTrue(3 > 2);
+    }
 }
