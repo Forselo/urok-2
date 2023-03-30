e3d8620eaa2cb7f74276f6c7d4eb6021dd67d6c0
Forselo <forelo@gmail.com>
2023-03-30 21:15:31 +0300

    fix metod

--- src/test/java/com/demoqa/PracticeForm.java
+++ src/test/java/com/demoqa/PracticeForm.java
@@ -14,16 +14,16 @@ public class PracticeForm extends TestBase {
         $("#firstName").setValue("Seva");
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("aaa@a.com");
-        $(byText("Male")).click();
+        $("#gender-radio-1").parent().click();
         $("#userNumber").setValue("7986957850");
         $("#dateOfBirthInput").sendKeys(CONTROL + "A");
         $("#dateOfBirthInput").sendKeys("11.11.1991");
         $("#dateOfBirthInput").pressEnter();
         $("#subjectsInput").setValue("Arts");
         $("#subjectsInput").pressEnter();
-        $(byText("Sports")).click();
-        $(byText("Reading")).click();
-        $(byText("Music")).click();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#hobbies-checkbox-2").parent().click();
+        $("#hobbies-checkbox-3").parent().click();
         $("#uploadPicture").uploadFile(new File("src/test/resources/images/1.jpg"));
         $("#currentAddress").setValue("Kazan");
         $("#state").click();
@@ -33,6 +33,9 @@ public class PracticeForm extends TestBase {
         $("#submit").click();
         $(".table").shouldHave(text("Seva Ivanov"), text("aaa@a.com"), text("Male"), text("7986957850"),
                 text("11 November,1991"), text("Arts"), text("Sports, Reading, Music"), text("1.jpg"), text("Kazan"), text("Haryana Karnal"));
+
+
+
 
 
     }
